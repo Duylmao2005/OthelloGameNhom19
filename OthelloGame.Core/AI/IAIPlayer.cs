8986ec2b6ea6a046e1d47a90db1737989bc3eca6
Duylmao2005 <duy231230731@lms.utc.edu.vn>
2026-04-15 14:49:00 +0700

    them AI

---
+++ OthelloGame.Core/AI/IAIPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OthelloGame.Core.AI
+{
+    internal interface IAIPlayer
+    {
+    }
+}
